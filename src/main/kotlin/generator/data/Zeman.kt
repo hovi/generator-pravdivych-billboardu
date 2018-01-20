abbe37cc2211705659301e293e0c8e3b88466e75
Kikinek <35631557+Kikinek@users.noreply.github.com>
2018-01-20 16:48:01 +0100

    Update Zeman.kt

--- src/main/kotlin/generator/data/Zeman.kt
+++ src/main/kotlin/generator/data/Zeman.kt
@@ -15,7 +15,14 @@ val respectedPeople = arrayOf(
         "Martin Konvička",
         "František Ringo Čech",
         "Tereza Pergnerová",
-        "Dan Hůlka"
+        "Dan Hůlka",
+        "Andrej Babiš",
+        "Vaše babička",
+        "KSČM",
+        "Zdeněk Troška",
+        "SPD",
+        "Tomio Okamura",
+        "ANO",
 )
 
 val badGuys = arrayOf(
@@ -26,14 +33,41 @@ val badGuys = arrayOf(
         "podnikatelům",
         "kavárníkům",
         "Kalouskovi",
-        "chemtrails"
+        "chemtrails",
+        "intelektuálům",
+        "slušnosti",
+        "lidskosti",
+        "evangelíkům",
+        "novinářům",
+        "víře",
+        "solidaritě",
+        "rozumu",
+        "abstinentům",
+        "spravedlnosti",
+        "soudnosti",
+        "Třetí velmoci",
+        "Pussy Riot",
+        "akademikům",
+        "vzdělancům",
+        "aktivistům",
+        "důstojnosti",
+        "fotbalovým fanouškům",
+        "vegetariánům",
+        "veganům",
+        
+        
+        
 )
 
 val patrioticLine = arrayOf(
         "Tato země\n je naše!",
         "Zemi si vzít\n nenecháme!",
         "Proti \nislámu!",
-        "Za naše děti!"
+        "Za naše děti!",
+        "Za naše pejsky!",
+        "Svou zemi vám nedáme!",
+        "Za Vysočinu!",
+        "Za Bechera!",
 )
 
 
@@ -41,18 +75,36 @@ val lovingFooter = arrayOf(
         "Volte \nZemana!",
         "Nevolte \nDrahoše!",
         "Zeman \nna Hrad!",
-        "Zeman \nznovu!"
+        "Zeman \nznovu!",
+        "Volte \nTrumpa!",
+        "Volte \nKreml!",
+        "Volte \nLosnu!",
+        "Volte \nMažňáka!",
+        "Losnu \nnebo Mažňáka",
+        "Volte \nBažňáka!",
+        "Volte \nkrále Vysočiny!",
 )
 
 val lovingHeader = arrayOf(
         "Náš prezident",
-        "Srdcem"
+        "Srdcem",
+        "Rozumem",
+        "Citečky",
+        "Nemyslete!",
 )
 
 val content = """
 content = <simples> +++ <footer>
 content = Stop +++ <bad-guys> a\n <bad-guys> +++ <footer>
 content = Stop +++ lžím, strachu a\n nenávisti +++\n<footer>
+content = Stop +++ vulgaritě a\n dezinformaci +++\n<footer>
+content = Stop +++ vulgaritě a\n neurvalosti +++\n<footer>
+content = Stop +++ zednářům a\n Římskému klubu +++\n<footer>
+content = Stop +++ Moskvě a\n Kremlu +++\n<footer>
+content = Stop +++ hlupákům a\n omezencům +++\n<footer>
+content = Stop +++ satarým strukturám +++\n<footer>
+content = Stop +++ hlupákům a\n populistům +++\n<footer>
+content = Stop +++ Nejedlému, Mynářovi a\n dalším přátelům +++\n<footer>
 content = <loving-header> +++ Volí ho i\n <respected-people> +++ <footer>
 
 
